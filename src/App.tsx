88d7dbedede3f1712573aeef4ef396838bc684dc
kephalosk <watchfromtheshore@gmail.com>
2025-05-16 11:01:49 +0200

    test QuizButton and DarkModeSwitch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,61 @@
 import "./App.scss";
 import React, { ReactElement } from "react";
+import QuizButton from "@/components/container/QuizButton/QuizButton.tsx";
+import { QuestionPositionEnum } from "@/globals/models/enums/QuestionPositionEnum.ts";
+import { QuestionStatusEnum } from "@/globals/models/enums/QuestionStatusEnum.ts";
+import Footer from "@/components/atoms/Footer/Footer.tsx";
 import DarkModeSwitch from "@/components/container/DarkModeSwitch/DarkModeSwitch.tsx";
 
 const App: React.FC = (): ReactElement => {
-
   return (
     <div className="app">
-      <DarkModeSwitch/>
+      <DarkModeSwitch />
+      <QuizButton
+        text={"4.5 : 1"}
+        position={QuestionPositionEnum.A}
+        status={QuestionStatusEnum.DEFAULT}
+        handleButtonClick={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        isDisabled={false}
+      />
+      <QuizButton
+        text={"4.5 : 1"}
+        position={QuestionPositionEnum.A}
+        status={QuestionStatusEnum.SELECTED}
+        handleButtonClick={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        isDisabled={false}
+      />
+      <QuizButton
+        text={"4.5 : 1"}
+        position={QuestionPositionEnum.A}
+        status={QuestionStatusEnum.WRONG}
+        handleButtonClick={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        isDisabled={false}
+      />
+      <QuizButton
+        text={"4.5 : 1"}
+        position={QuestionPositionEnum.A}
+        status={QuestionStatusEnum.CORRECTED}
+        handleButtonClick={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        isDisabled={false}
+      />
+      <QuizButton
+        text={"4.5 : 1"}
+        position={QuestionPositionEnum.A}
+        status={QuestionStatusEnum.RIGHT}
+        handleButtonClick={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        isDisabled={false}
+      />
+      <Footer />
     </div>
   );
 };
