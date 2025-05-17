99cf28af01dfc22afa1676ad4919365bb33a5fca
kephalosk <watchfromtheshore@gmail.com>
2025-05-17 04:45:29 +0200

    clean up atom components

--- src/components/atoms/Slider/SliderBarValue/SliderBarValue.tsx
+++
@@ -1,8 +0,0 @@
-import "./SliderBarValue.scss";
-import React, { ReactElement } from "react";
-
-const SliderBarValue: React.FC = (): ReactElement => {
-  return <div className="sliderBarValue"></div>;
-};
-
-export default SliderBarValue;
