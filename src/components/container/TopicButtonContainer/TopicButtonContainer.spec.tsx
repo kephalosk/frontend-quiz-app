7ff6fd9cc3eff653814d99c18247ff0717276b77
kephalosk <watchfromtheshore@gmail.com>
2025-05-18 18:01:03 +0200

    add component TopicButtonContainer

---
+++ src/components/container/TopicButtonContainer/TopicButtonContainer.spec.tsx
@@ -0,0 +1,99 @@
+import { ReactElement } from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { TopicEnum } from "@/globals/models/enums/TopicEnum.ts";
+import { TopicItem, TopicItems } from "@/globals/constants/TopicItems.ts";
+import TopicButtonContainer from "@/components/container/TopicButtonContainer/TopicButtonContainer.tsx";
+import useUpdateTopic from "@/hooks/redux/topic/useUpdateTopic.ts";
+import TopicButton from "@/components/container/TopicButton/TopicButton.tsx";
+
+const topicMock: TopicEnum = TopicEnum.HTML;
+const topicButtonDataTestId: string = "topic-button";
+jest.mock(
+  "@/components/container/TopicButton/TopicButton.tsx",
+  (): jest.Mock =>
+    jest.fn((props): ReactElement => {
+      return (
+        <button
+          data-testid={topicButtonDataTestId}
+          onClick={() => props.handleButtonClick(topicMock)}
+        >
+          {props.text}
+        </button>
+      );
+    }),
+);
+
+jest.mock(
+  "@/hooks/redux/topic/useUpdateTopic.ts",
+  (): {
+    __esModule: boolean;
+    default: jest.Mock;
+  } => ({
+    __esModule: true,
+    default: jest.fn(),
+  }),
+);
+
+describe("TopicButtonContainer Component", (): void => {
+  const setup = (): { container: HTMLElement } => {
+    return render(<TopicButtonContainer />);
+  };
+
+  const useUpdateTopicMock: jest.Mock = jest.fn();
+
+  beforeEach((): void => {
+    (useUpdateTopic as jest.Mock).mockReturnValue(useUpdateTopicMock);
+  });
+
+  it(`renders div topicButtonContainer`, (): void => {
+    const { container } = setup();
+
+    const element: HTMLElement | null = container.querySelector(
+      ".topicButtonContainer",
+    );
+
+    expect(element).toBeInTheDocument();
+  });
+
+  it("renders components TopicButton", (): void => {
+    setup();
+
+    const elements: HTMLElement[] = screen.getAllByTestId(
+      topicButtonDataTestId,
+    );
+
+    expect(elements).toHaveLength(TopicItems.length);
+    expect(TopicButton).toHaveBeenCalledTimes(TopicItems.length);
+    TopicItems.forEach((item: TopicItem, index: number) => {
+      expect(TopicButton).toHaveBeenNthCalledWith(
+        index + 1,
+        {
+          type: item.topic,
+          text: item.text,
+          handleButtonClick: expect.any(Function),
+        },
+        undefined,
+      );
+    });
+  });
+
+  it("calls handleTopicUpdate when TopicButton is clicked", (): void => {
+    setup();
+
+    const elements: HTMLElement[] = screen.getAllByTestId(
+      topicButtonDataTestId,
+    );
+    fireEvent.click(elements.at(0)!);
+
+    expect(useUpdateTopicMock).toHaveBeenCalledTimes(1);
+    expect(useUpdateTopicMock).toHaveBeenCalledWith(topicMock);
+  });
+
+  it("calls hook useUpdateTopic", (): void => {
+    setup();
+
+    expect(useUpdateTopic).toHaveBeenCalledTimes(1);
+    expect(useUpdateTopic).toHaveBeenCalledWith();
+    expect(useUpdateTopic).toHaveReturnedWith(useUpdateTopicMock);
+  });
+});
---
+++ src/components/container/TopicButtonContainer/TopicButtonContainer.tsx
@@ -0,0 +1,27 @@
+import "./TopicButtonContainer.scss";
+import React, { ReactElement } from "react";
+import { TopicItem, TopicItems } from "@/globals/constants/TopicItems.ts";
+import TopicButton from "@/components/container/TopicButton/TopicButton.tsx";
+import useUpdateTopic from "@/hooks/redux/topic/useUpdateTopic.ts";
+import { TopicEnum } from "@/globals/models/enums/TopicEnum.ts";
+
+const TopicButtonContainer: React.FC = (): ReactElement => {
+  const handleTopicUpdate: (newValue: TopicEnum) => void = useUpdateTopic();
+
+  return (
+    <div className="topicButtonContainer">
+      {TopicItems.map(
+        (topic: TopicItem, index: number): ReactElement => (
+          <TopicButton
+            key={index}
+            text={topic.text}
+            type={topic.topic}
+            handleButtonClick={() => handleTopicUpdate(topic.topic)}
+          />
+        ),
+      )}
+    </div>
+  );
+};
+
+export default TopicButtonContainer;
