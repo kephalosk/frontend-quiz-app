bbcf6258e3107487611d2b762589b7e7d96d060e
kephalosk <watchfromtheshore@gmail.com>
2025-05-18 19:00:13 +0200

    remove unused utils

--- src/globals/utils/correctValuesThatAreTooSmall.ts
+++
@@ -1,8 +0,0 @@
-const correctValuesThatAreTooSmall = (
-  newValue: number,
-  minValue: number,
-): number => {
-  return newValue < minValue ? minValue : newValue;
-};
-
-export default correctValuesThatAreTooSmall;
