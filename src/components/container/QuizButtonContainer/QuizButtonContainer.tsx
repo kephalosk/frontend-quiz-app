90eacdec32a3b5271b4403f7745bce3e9648ac2f
kephalosk <watchfromtheshore@gmail.com>
2025-05-19 16:41:38 +0200

    create dummies for QuizButtonContainer

---
+++ src/components/container/QuizButtonContainer/QuizButtonContainer.tsx
@@ -0,0 +1,27 @@
+import "./TopicButtonContainer.scss";
+import React, { ReactElement } from "react";
+import { TopicItem, TopicItems } from "@/globals/constants/TopicItems.ts";
+import TopicButton from "@/components/container/TopicButton/TopicButton.tsx";
+import useUpdateTopic from "@/hooks/redux/topic/useUpdateTopic.ts";
+import { TopicEnum } from "@/globals/models/enums/TopicEnum.ts";
+
+const QuizButtonContainer: React.FC = (): ReactElement => {
+  const handleTopicUpdate: (newValue: TopicEnum) => void = useUpdateTopic();
+
+  return (
+    <div className="quizButtonContainer">
+      {TopicItems.map(
+        (topic: TopicItem, index: number): ReactElement => (
+          <TopicButton
+            key={index}
+            text={topic.text}
+            type={topic.topic}
+            handleButtonClick={() => handleTopicUpdate(topic.topic)}
+          />
+        ),
+      )}
+    </div>
+  );
+};
+
+export default QuizButtonContainer;
