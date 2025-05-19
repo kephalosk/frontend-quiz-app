d613fc2808d9004567fb25d850d60de58f1dfe13
kephalosk <watchfromtheshore@gmail.com>
2025-05-19 15:55:25 +0200

    refactor shuffleArray

--- src/globals/utils/shuffleArray.ts
+++ src/globals/utils/shuffleArray.ts
@@ -2,11 +2,17 @@ function shuffleArray<T>(array: T[]): T[] {
   const shuffledArray: T[] = array.slice();
   let isShuffled: boolean = false;
   do {
-    for (let i: number = shuffledArray.length - 1; i > 0; i--) {
-      const j: number = Math.floor(Math.random() * (i + 1));
-      [shuffledArray[i], shuffledArray[j]] = [
-        shuffledArray[j],
-        shuffledArray[i],
+    for (
+      let currentIndex: number = shuffledArray.length - 1;
+      currentIndex > 0;
+      currentIndex--
+    ) {
+      const indexToSwapWith: number = Math.floor(
+        Math.random() * (currentIndex + 1),
+      );
+      [shuffledArray[currentIndex], shuffledArray[indexToSwapWith]] = [
+        shuffledArray[indexToSwapWith],
+        shuffledArray[currentIndex],
       ];
     }
     if (JSON.stringify(shuffledArray) !== JSON.stringify(array)) {
