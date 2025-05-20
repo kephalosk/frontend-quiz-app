46ef22ab05ea4d5d349ef424edf5184d41dd3047
kephalosk <watchfromtheshore@gmail.com>
2025-05-20 18:26:23 +0200

    add component QuizButtonContainer

--- src/components/container/QuizButtonContainer/QuizButtonContainer.spec.tsx
+++ src/components/container/QuizButtonContainer/QuizButtonContainer.spec.tsx
@@ -1,30 +1,23 @@
 import { ReactElement } from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
-import { TopicEnum } from "@/globals/models/enums/TopicEnum.ts";
-import { TopicItem, TopicItems } from "@/globals/constants/TopicItems.ts";
-import TopicButtonContainer from "@/components/container/TopicButtonContainer/TopicButtonContainer.tsx";
-import useUpdateTopic from "@/hooks/redux/topic/dispatch/useUpdateTopic.ts";
-import TopicButton from "@/components/container/TopicButton/TopicButton.tsx";
-
-const topicMock: TopicEnum = TopicEnum.HTML;
-const topicButtonDataTestId: string = "topic-button";
-jest.mock(
-  "@/components/container/TopicButton/TopicButton.tsx",
-  (): jest.Mock =>
-    jest.fn((props): ReactElement => {
-      return (
-        <button
-          data-testid={topicButtonDataTestId}
-          onClick={() => props.handleButtonClick(topicMock)}
-        >
-          {props.text}
-        </button>
-      );
-    }),
-);
+import QuizButtonContainer, {
+  QuizButtonContainerProps,
+} from "@/components/container/QuizButtonContainer/QuizButtonContainer.tsx";
+import { EPQuestion } from "@/globals/models/entrypoints/EPQuestion.ts";
+import { QuestionStatusEnum } from "@/globals/models/enums/QuestionStatusEnum.ts";
+import { UpdateSelectionHook } from "@/globals/models/types/QuizTypes.ts";
+import { QuestionPositionEnum } from "@/globals/models/enums/QuestionPositionEnum.ts";
+import { DefaultQuestionStatusArray } from "@/globals/constants/DefaultQuestionStatusArray.ts";
+import { mockedQuestions } from "@/redux/fixtures/mockedQuestions.ts";
+import QuizButton from "@/components/container/QuizButton/QuizButton.tsx";
+import useQuestions from "@/hooks/redux/topic/selector/useQuestions.ts";
+import useCurrentIndex from "@/hooks/redux/topic/selector/useCurrentIndex.ts";
+import useUpdateSelection from "@/hooks/quiz/useUpdateSelection.ts";
+import getCurrentStatusArray from "@/hooks/quiz/getCurrentStatusArray.ts";
+import getQuestionPositionByIndex from "@/globals/helper/getQuestionPositionByIndex.ts";
 
 jest.mock(
-  "@/hooks/redux/topic/dispatch/useUpdateTopic.ts",
+  "@/hooks/redux/topic/selector/useQuestions.ts",
   (): {
     __esModule: boolean;
     default: jest.Mock;
@@ -34,66 +27,212 @@ jest.mock(
   }),
 );
 
-describe("TopicButtonContainer Component", (): void => {
-  const setup = (): { container: HTMLElement } => {
-    return render(<TopicButtonContainer />);
+jest.mock(
+  "@/hooks/redux/topic/selector/useCurrentIndex.ts",
+  (): {
+    __esModule: boolean;
+    default: jest.Mock;
+  } => ({
+    __esModule: true,
+    default: jest.fn(),
+  }),
+);
+
+jest.mock(
+  "@/hooks/quiz/useUpdateSelection.ts",
+  (): {
+    __esModule: boolean;
+    default: jest.Mock;
+  } => ({
+    __esModule: true,
+    default: jest.fn(),
+  }),
+);
+
+jest.mock(
+  "@/hooks/quiz/getCurrentStatusArray.ts",
+  (): {
+    __esModule: boolean;
+    default: jest.Mock;
+  } => ({
+    __esModule: true,
+    default: jest.fn(),
+  }),
+);
+
+jest.mock(
+  "@/globals/helper/getQuestionPositionByIndex.ts",
+  (): {
+    __esModule: boolean;
+    default: jest.Mock;
+  } => ({
+    __esModule: true,
+    default: jest.fn(),
+  }),
+);
+
+const answerMock: string = "answer";
+const positionClickedMock: QuestionPositionEnum = QuestionPositionEnum.A;
+const quizButtonDataTestId: string = "quiz-button";
+jest.mock(
+  "@/components/container/QuizButton/QuizButton.tsx",
+  (): jest.Mock =>
+    jest.fn((props): ReactElement => {
+      return (
+        <button
+          data-testid={quizButtonDataTestId}
+          onClick={() => props.propagateAnswer(answerMock, positionClickedMock)}
+        ></button>
+      );
+    }),
+);
+
+describe("QuizButtonContainer Component", (): void => {
+  const isQuestionAnswered: boolean = false;
+  const propagateCorrectSelectionMock: jest.Mock = jest.fn();
+  const setup = (
+    propsOverride?: Partial<QuizButtonContainerProps>,
+  ): { container: HTMLElement } => {
+    const defaultProps: QuizButtonContainerProps = {
+      isQuestionAnswered,
+      propagateCorrectSelection: propagateCorrectSelectionMock,
+    };
+    const props = { ...defaultProps, ...propsOverride };
+    return render(<QuizButtonContainer {...props} />);
   };
 
-  const useUpdateTopicMock: jest.Mock = jest.fn();
+  const questionsMock: EPQuestion[] = mockedQuestions;
+
+  const currentIndexMock: number = 0;
+
+  const statusArraySelectedMock: QuestionStatusEnum[] =
+    DefaultQuestionStatusArray;
+  const statusArrayAnsweredMock: QuestionStatusEnum[] =
+    DefaultQuestionStatusArray;
+  const handleSelectionMock: jest.Mock = jest.fn();
+  const useUpdateSelectionMock: UpdateSelectionHook = {
+    statusArraySelected: statusArraySelectedMock,
+    statusArrayAnswered: statusArrayAnsweredMock,
+    handleSelection: handleSelectionMock,
+  };
+
+  const currentStatusArrayMock: QuestionStatusEnum[] =
+    DefaultQuestionStatusArray;
+
+  const getQuestionPositionByIndexMock: QuestionPositionEnum =
+    QuestionPositionEnum.A;
 
   beforeEach((): void => {
-    (useUpdateTopic as jest.Mock).mockReturnValue(useUpdateTopicMock);
+    (useQuestions as jest.Mock).mockReturnValue(questionsMock);
+    (useCurrentIndex as jest.Mock).mockReturnValue(currentIndexMock);
+    (useUpdateSelection as jest.Mock).mockReturnValue(useUpdateSelectionMock);
+    (getCurrentStatusArray as jest.Mock).mockReturnValue(
+      currentStatusArrayMock,
+    );
+    (getQuestionPositionByIndex as jest.Mock).mockReturnValue(
+      getQuestionPositionByIndexMock,
+    );
   });
 
-  it(`renders div topicButtonContainer`, (): void => {
+  it(`renders div quizButtonContainer`, (): void => {
     const { container } = setup();
 
     const element: HTMLElement | null = container.querySelector(
-      ".topicButtonContainer",
+      ".quizButtonContainer",
     );
 
     expect(element).toBeInTheDocument();
   });
 
-  it("renders components TopicButton", (): void => {
+  it("renders components QuizButton from currentQuestion", (): void => {
+    const currentOptions: string[] = questionsMock[currentIndexMock].options;
     setup();
 
-    const elements: HTMLElement[] = screen.getAllByTestId(
-      topicButtonDataTestId,
-    );
+    const elements: HTMLElement[] = screen.getAllByTestId(quizButtonDataTestId);
 
-    expect(elements).toHaveLength(TopicItems.length);
-    expect(TopicButton).toHaveBeenCalledTimes(TopicItems.length);
-    TopicItems.forEach((item: TopicItem, index: number) => {
-      expect(TopicButton).toHaveBeenNthCalledWith(
+    expect(elements).toHaveLength(currentOptions.length);
+    expect(QuizButton).toHaveBeenCalledTimes(currentOptions.length);
+    currentOptions.forEach((option: string, index: number) => {
+      expect(QuizButton).toHaveBeenNthCalledWith(
         index + 1,
         {
-          type: item.topic,
-          text: item.text,
-          handleButtonClick: expect.any(Function),
+          text: option,
+          position: getQuestionPositionByIndexMock,
+          status: currentStatusArrayMock[index],
+          propagateAnswer: handleSelectionMock,
+          isDisabled: isQuestionAnswered,
         },
         undefined,
       );
     });
   });
 
-  it("calls handleTopicUpdate when TopicButton is clicked", (): void => {
+  it("does not render components QuizButton when currentQuestion is undefined", (): void => {
+    const questionsUndefined: EPQuestion[] = [
+      undefined as unknown as EPQuestion,
+      undefined as unknown as EPQuestion,
+      undefined as unknown as EPQuestion,
+      undefined as unknown as EPQuestion,
+    ];
+    (useQuestions as jest.Mock).mockReturnValue(questionsUndefined);
     setup();
 
-    const elements: HTMLElement[] = screen.getAllByTestId(
-      topicButtonDataTestId,
-    );
-    fireEvent.click(elements.at(0)!);
+    const elements: HTMLElement[] =
+      screen.queryAllByTestId(quizButtonDataTestId);
 
-    expect(useUpdateTopicMock).toHaveBeenCalledTimes(1);
-    expect(useUpdateTopicMock).toHaveBeenCalledWith(topicMock);
+    expect(elements).toHaveLength(0);
+    expect(QuizButton).not.toHaveBeenCalled();
   });
 
-  it("calls hook useUpdateTopic", (): void => {
+  it("calls handleSelection when QuizButton is triggered", (): void => {
     setup();
 
-    expect(useUpdateTopic).toHaveBeenCalledTimes(1);
-    expect(useUpdateTopic).toHaveBeenCalledWith();
-    expect(useUpdateTopic).toHaveReturnedWith(useUpdateTopicMock);
+    const elements: HTMLElement[] = screen.getAllByTestId(quizButtonDataTestId);
+    fireEvent.click(elements.at(0)!);
+
+    expect(handleSelectionMock).toHaveBeenCalledTimes(1);
+    expect(handleSelectionMock).toHaveBeenCalledWith(
+      answerMock,
+      positionClickedMock,
+    );
+  });
+
+  it("calls hook useQuestions", (): void => {
+    setup();
+
+    expect(useQuestions).toHaveBeenCalledTimes(1);
+    expect(useQuestions).toHaveBeenCalledWith();
+    expect(useQuestions).toHaveReturnedWith(questionsMock);
+  });
+
+  it("calls hook useCurrentIndex", (): void => {
+    setup();
+
+    expect(useCurrentIndex).toHaveBeenCalledTimes(1);
+    expect(useCurrentIndex).toHaveBeenCalledWith();
+    expect(useCurrentIndex).toHaveReturnedWith(currentIndexMock);
+  });
+
+  it("calls hook useUpdateSelection", (): void => {
+    setup();
+
+    expect(useUpdateSelection).toHaveBeenCalledTimes(1);
+    expect(useUpdateSelection).toHaveBeenCalledWith(
+      questionsMock[currentIndexMock],
+      propagateCorrectSelectionMock,
+    );
+    expect(useUpdateSelection).toHaveReturnedWith(useUpdateSelectionMock);
+  });
+
+  it("calls hook getCurrentStatusArray", (): void => {
+    setup();
+
+    expect(getCurrentStatusArray).toHaveBeenCalledTimes(1);
+    expect(getCurrentStatusArray).toHaveBeenCalledWith(
+      isQuestionAnswered,
+      statusArrayAnsweredMock,
+      statusArraySelectedMock,
+    );
+    expect(getCurrentStatusArray).toHaveReturnedWith(currentStatusArrayMock);
   });
 });
--- src/components/container/QuizButtonContainer/QuizButtonContainer.tsx
+++ src/components/container/QuizButtonContainer/QuizButtonContainer.tsx
@@ -1,25 +1,57 @@
-import "./TopicButtonContainer.scss";
+import "./QuizButtonContainer.scss";
 import React, { ReactElement } from "react";
-import { TopicItem, TopicItems } from "@/globals/constants/TopicItems.ts";
-import TopicButton from "@/components/container/TopicButton/TopicButton.tsx";
-import useUpdateTopic from "@/hooks/redux/topic/dispatch/useUpdateTopic.ts";
-import { TopicEnum } from "@/globals/models/enums/TopicEnum.ts";
+import { EPQuestion } from "@/globals/models/entrypoints/EPQuestion.ts";
+import useQuestions from "@/hooks/redux/topic/selector/useQuestions.ts";
+import QuizButton from "@/components/container/QuizButton/QuizButton.tsx";
+import useCurrentIndex from "@/hooks/redux/topic/selector/useCurrentIndex.ts";
+import getQuestionPositionByIndex from "@/globals/helper/getQuestionPositionByIndex.ts";
+import { UpdateSelectionHook } from "@/globals/models/types/QuizTypes.ts";
+import useUpdateSelection from "@/hooks/quiz/useUpdateSelection.ts";
+import getCurrentStatusArray from "@/hooks/quiz/getCurrentStatusArray.ts";
+import { QuestionStatusEnum } from "@/globals/models/enums/QuestionStatusEnum.ts";
 
-const QuizButtonContainer: React.FC = (): ReactElement => {
-  const handleTopicUpdate: (newValue: TopicEnum) => void = useUpdateTopic();
+export interface QuizButtonContainerProps {
+  isQuestionAnswered: boolean;
+  propagateCorrectSelection: (IsSelectedAnswerCorrect: boolean) => void;
+}
+
+const QuizButtonContainer: React.FC<QuizButtonContainerProps> = ({
+  isQuestionAnswered,
+  propagateCorrectSelection,
+}: QuizButtonContainerProps): ReactElement => {
+  const questions: EPQuestion[] = useQuestions();
+  const currentIndex: number = useCurrentIndex();
+  const currentQuestion: EPQuestion = questions[currentIndex];
+  const {
+    statusArraySelected,
+    statusArrayAnswered,
+    handleSelection,
+  }: UpdateSelectionHook = useUpdateSelection(
+    currentQuestion,
+    propagateCorrectSelection,
+  );
+  const currentStatusArray: QuestionStatusEnum[] = getCurrentStatusArray(
+    isQuestionAnswered,
+    statusArrayAnswered,
+    statusArraySelected,
+  );
 
   return (
     <div className="quizButtonContainer">
-      {TopicItems.map(
-        (topic: TopicItem, index: number): ReactElement => (
-          <TopicButton
-            key={index}
-            text={topic.text}
-            type={topic.topic}
-            handleButtonClick={() => handleTopicUpdate(topic.topic)}
-          />
-        ),
-      )}
+      {currentQuestion &&
+        currentQuestion.options &&
+        currentQuestion.options.map(
+          (question: string, index: number): ReactElement => (
+            <QuizButton
+              key={index}
+              text={question}
+              position={getQuestionPositionByIndex(index)}
+              status={currentStatusArray[index]}
+              propagateAnswer={handleSelection}
+              isDisabled={isQuestionAnswered}
+            />
+          ),
+        )}
     </div>
   );
 };
