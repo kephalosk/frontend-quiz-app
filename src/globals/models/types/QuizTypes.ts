fd2ee1c643f792bccad3cd462d3676cd23c95187
kephalosk <watchfromtheshore@gmail.com>
2025-05-22 16:41:39 +0200

    add QuizTypes

--- src/globals/models/types/QuizTypes.ts
+++ src/globals/models/types/QuizTypes.ts
@@ -9,3 +9,20 @@ export type UpdateSelectionHook = {
     positionClicked: QuestionPositionEnum,
   ) => void;
 };
+
+export type QuizSubmitHook = {
+  handleSubmit: () => void;
+  updateSelectionResult: (value: boolean) => void;
+  resetKey: number;
+  isQuestionAnswered: boolean;
+  isQuizFinished: boolean;
+  hasError: boolean;
+};
+
+export type SubmitAnswerHook = { tryToSubmitAnswer: () => void };
+
+export type ResetKeyHook = { resetKey: number; resetStatusArray: () => void };
+
+export type UpdateSelectionResultHook = {
+  updateSelectionResult: (value: boolean) => void;
+};
