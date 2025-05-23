83f1ad45a1b2a8556e9e2fcab6c9024c2b00104b
kephalosk <watchfromtheshore@gmail.com>
2025-05-23 13:09:30 +0200

    add hook useCurrentQuestion

---
+++ src/hooks/quiz/useCurrentQuestion.spec.tsx
@@ -0,0 +1,173 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { CurrentQuestionHook } from "@/globals/models/types/QuizTypes.ts";
+import useQuestions from "@/hooks/redux/topic/selector/useQuestions.ts";
+import useCurrentIndex from "@/hooks/redux/topic/selector/useCurrentIndex.ts";
+import useProgressInfo from "@/hooks/quiz/useProgressInfo.ts";
+import useProgressPerCent from "@/hooks/quiz/useProgressPerCent.ts";
+import useCurrentQuestion from "@/hooks/quiz/useCurrentQuestion.ts";
+import { EPQuestion } from "@/globals/models/entrypoints/EPQuestion.ts";
+import { mockedQuestions } from "@/redux/fixtures/mockedQuestions.ts";
+
+jest.mock(
+  "@/hooks/redux/topic/selector/useQuestions.ts",
+  (): {
+    __esModule: boolean;
+    default: jest.Mock;
+  } => ({
+    __esModule: true,
+    default: jest.fn(),
+  }),
+);
+
+jest.mock(
+  "@/hooks/redux/topic/selector/useCurrentIndex.ts",
+  (): {
+    __esModule: boolean;
+    default: jest.Mock;
+  } => ({
+    __esModule: true,
+    default: jest.fn(),
+  }),
+);
+
+jest.mock(
+  "@/hooks/quiz/useProgressInfo.ts",
+  (): {
+    __esModule: boolean;
+    default: jest.Mock;
+  } => ({
+    __esModule: true,
+    default: jest.fn(),
+  }),
+);
+
+jest.mock(
+  "@/hooks/quiz/useProgressPerCent.ts",
+  (): {
+    __esModule: boolean;
+    default: jest.Mock;
+  } => ({
+    __esModule: true,
+    default: jest.fn(),
+  }),
+);
+
+const testComponentDataTestId: string = "test-component";
+const testComponentDataTestIdQuestion: string = "test-component-question";
+const testComponentDataTestIdProgressInfo: string =
+  "test-component-progress-info";
+const testComponentDataTestIdProgressPerCent: string =
+  "test-component-progress-per-cent";
+const TestComponent: React.FC = () => {
+  const {
+    currentQuestionText,
+    progressInfo,
+    progressPerCent,
+  }: CurrentQuestionHook = useCurrentQuestion();
+
+  return (
+    <div data-testid={testComponentDataTestId}>
+      <div
+        data-testid={testComponentDataTestIdQuestion}
+      >{`${currentQuestionText}`}</div>
+      <div
+        data-testid={testComponentDataTestIdProgressInfo}
+      >{`${progressInfo}`}</div>
+      <div
+        data-testid={testComponentDataTestIdProgressPerCent}
+      >{`${progressPerCent}`}</div>
+    </div>
+  );
+};
+
+describe("useCurrentQuestion Hook", (): void => {
+  const setup = (): void => {
+    render(<TestComponent />);
+  };
+
+  const questionsMock: EPQuestion[] = mockedQuestions;
+
+  const currentIndexMock: number = 0;
+
+  const progressInfoMock: string = "progressInfo";
+
+  const progressPerCentMock: number = 50;
+
+  beforeEach((): void => {
+    (useQuestions as jest.Mock).mockReturnValue(questionsMock);
+    (useCurrentIndex as jest.Mock).mockReturnValue(currentIndexMock);
+    (useProgressInfo as jest.Mock).mockReturnValue(progressInfoMock);
+    (useProgressPerCent as jest.Mock).mockReturnValue(progressPerCentMock);
+  });
+
+  it("returns currentQuestionText", (): void => {
+    setup();
+
+    const element: HTMLElement = screen.getByTestId(
+      testComponentDataTestIdQuestion,
+    );
+
+    expect(element.textContent).toEqual(
+      questionsMock[currentIndexMock].question,
+    );
+  });
+
+  it("returns progressInfo", (): void => {
+    setup();
+
+    const element: HTMLElement = screen.getByTestId(
+      testComponentDataTestIdProgressInfo,
+    );
+
+    expect(element.textContent).toEqual(progressInfoMock);
+  });
+
+  it("returns progressPerCent", (): void => {
+    setup();
+
+    const element: HTMLElement = screen.getByTestId(
+      testComponentDataTestIdProgressPerCent,
+    );
+
+    expect(element.textContent).toEqual(`${progressPerCentMock}`);
+  });
+
+  it("calls hook useQuestions", (): void => {
+    setup();
+
+    expect(useQuestions).toHaveBeenCalledTimes(1);
+    expect(useQuestions).toHaveBeenCalledWith();
+    expect(useQuestions).toHaveReturnedWith(questionsMock);
+  });
+
+  it("calls hook useCurrentIndex", (): void => {
+    setup();
+
+    expect(useCurrentIndex).toHaveBeenCalledTimes(1);
+    expect(useCurrentIndex).toHaveBeenCalledWith();
+    expect(useCurrentIndex).toHaveReturnedWith(currentIndexMock);
+  });
+
+  it("calls hook useProgressInfo", (): void => {
+    setup();
+
+    expect(useProgressInfo).toHaveBeenCalledTimes(1);
+    expect(useProgressInfo).toHaveBeenCalledWith({
+      currentIndex: currentIndexMock,
+      totalQuestions: questionsMock.length,
+    });
+    expect(useProgressInfo).toHaveReturnedWith(progressInfoMock);
+  });
+
+  it("calls hook useProgressPerCent", (): void => {
+    setup();
+
+    expect(useProgressPerCent).toHaveBeenCalledTimes(1);
+    expect(useProgressPerCent).toHaveBeenCalledWith({
+      currentIndex: currentIndexMock,
+      totalQuestions: questionsMock.length,
+    });
+    expect(useProgressPerCent).toHaveReturnedWith(progressPerCentMock);
+  });
+});
---
+++ src/hooks/quiz/useCurrentQuestion.ts
@@ -0,0 +1,31 @@
+import { useMemo } from "react";
+import { EPQuestion } from "@/globals/models/entrypoints/EPQuestion.ts";
+import useQuestions from "@/hooks/redux/topic/selector/useQuestions.ts";
+import useCurrentIndex from "@/hooks/redux/topic/selector/useCurrentIndex.ts";
+import useProgressInfo from "@/hooks/quiz/useProgressInfo.ts";
+import useProgressPerCent from "@/hooks/quiz/useProgressPerCent.ts";
+import { CurrentQuestionHook } from "@/globals/models/types/QuizTypes.ts";
+
+const useCurrentQuestion: () => CurrentQuestionHook =
+  (): CurrentQuestionHook => {
+    const questions: EPQuestion[] = useQuestions();
+    const currentIndex: number = useCurrentIndex();
+
+    const progressInfo: string = useProgressInfo({
+      currentIndex,
+      totalQuestions: questions.length,
+    });
+
+    const progressPerCent: number = useProgressPerCent({
+      currentIndex,
+      totalQuestions: questions.length,
+    });
+
+    const currentQuestionText: string = useMemo(
+      () => questions[currentIndex].question,
+      [currentIndex, questions],
+    );
+    return { currentQuestionText, progressInfo, progressPerCent };
+  };
+
+export default useCurrentQuestion;
