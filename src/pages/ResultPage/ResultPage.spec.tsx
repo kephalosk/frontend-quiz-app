3f29b837b1c4b3e9984b444eb2d991c170badf8c
kephalosk <watchfromtheshore@gmail.com>
2025-05-23 18:13:44 +0200

    add ResultPage

--- src/pages/ResultPage/ResultPage.spec.tsx
+++ src/pages/ResultPage/ResultPage.spec.tsx
@@ -1,17 +1,21 @@
 import { ReactElement } from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import TitleContainer from "@/components/container/TitleContainer/TitleContainer.tsx";
-import useRouterStartPage from "@/hooks/router/useRouterStartPage.ts";
-import StartPage from "@/pages/StartPage/StartPage.tsx";
-import TopicButtonContainer from "@/components/container/TopicButtonContainer/TopicButtonContainer";
 import {
-  STARTPAGE_HEADLINE_BOLD_TEXT,
-  STARTPAGE_HEADLINE_TEXT,
-  STARTPAGE_SUB_LINE_TEXT,
+  EMPTY_STRING,
+  PLAY_AGAIN_BUTTON_TEXT,
+  RESULTPAGE_HEADLINE_BOLD_TEXT,
+  RESULTPAGE_HEADLINE_TEXT,
 } from "@/globals/constants/Constants.ts";
+import useResetGame from "@/hooks/result/useResetGame.ts";
+import ScoreCard from "@/components/container/ScoreCard/ScoreCard.tsx";
+import SubmitButton from "@/components/atoms/SubmitButton/SubmitButton.tsx";
+import ResultPage from "@/pages/ResultPage/ResultPage.tsx";
+import { ResetGameHook } from "@/globals/models/types/ResultTypes.ts";
+import { ReactNamesEnum } from "@/globals/models/enums/ReactNamesEnum.ts";
 
 jest.mock(
-  "@/hooks/router/useRouterStartPage.ts",
+  "@/hooks/result/useResetGame.ts",
   (): {
     __esModule: boolean;
     default: jest.Mock;
@@ -30,24 +34,47 @@ jest.mock(
     }),
 );
 
-const topicButtonContainerDataTestId: string = "topic-button-container";
+const scoreCardDataTestId: string = "score-card";
 jest.mock(
-  "@/components/container/TopicButtonContainer/TopicButtonContainer",
+  "@/components/container/ScoreCard/ScoreCard.tsx",
   (): jest.Mock =>
     jest.fn((): ReactElement => {
-      return <div data-testid={topicButtonContainerDataTestId}></div>;
+      return <div data-testid={scoreCardDataTestId}></div>;
     }),
 );
 
-describe("StartPage Component", (): void => {
+const submitButtonContainerDataTestId: string = "submit-button";
+jest.mock(
+  "@/components/atoms/SubmitButton/SubmitButton.tsx",
+  (): jest.Mock =>
+    jest.fn((props): ReactElement => {
+      return (
+        <div
+          data-testid={submitButtonContainerDataTestId}
+          onClick={props.handleButtonClick}
+        ></div>
+      );
+    }),
+);
+
+describe("ResultPage Component", (): void => {
   const setup = (): { container: HTMLElement } => {
-    return render(<StartPage />);
+    return render(<ResultPage />);
   };
 
-  it(`renders div startPage`, (): void => {
+  const handleResetMock: jest.Mock = jest.fn();
+  const useResetGameMock: ResetGameHook = {
+    handleReset: handleResetMock,
+  };
+
+  beforeEach((): void => {
+    (useResetGame as jest.Mock).mockReturnValue(useResetGameMock);
+  });
+
+  it(`renders div resultPage`, (): void => {
     const { container } = setup();
 
-    const element: HTMLElement | null = container.querySelector(".startPage");
+    const element: HTMLElement | null = container.querySelector(".resultPage");
 
     expect(element).toBeInTheDocument();
   });
@@ -61,31 +88,68 @@ describe("StartPage Component", (): void => {
     expect(TitleContainer).toHaveBeenCalledTimes(1);
     expect(TitleContainer).toHaveBeenCalledWith(
       {
-        firstLine: STARTPAGE_HEADLINE_TEXT,
-        headline: STARTPAGE_HEADLINE_BOLD_TEXT,
-        subLine: STARTPAGE_SUB_LINE_TEXT,
+        firstLine: RESULTPAGE_HEADLINE_TEXT,
+        headline: RESULTPAGE_HEADLINE_BOLD_TEXT,
+        subLine: EMPTY_STRING,
       },
       undefined,
     );
   });
 
-  it("renders component TopicButtonContainer", (): void => {
+  it(`renders div resultPageResult`, (): void => {
+    const { container } = setup();
+
+    const element: HTMLElement | null =
+      container.querySelector(".resultPageResult");
+
+    expect(element).toBeInTheDocument();
+  });
+
+  it("renders component ScoreCard", (): void => {
+    setup();
+
+    const element: HTMLElement = screen.getByTestId(scoreCardDataTestId);
+
+    expect(element).toBeInTheDocument();
+    expect(ScoreCard).toHaveBeenCalledTimes(1);
+    expect(ScoreCard).toHaveBeenCalledWith({}, undefined);
+  });
+
+  it("renders component SubmitButton", (): void => {
     setup();
 
     const element: HTMLElement = screen.getByTestId(
-      topicButtonContainerDataTestId,
+      submitButtonContainerDataTestId,
     );
 
     expect(element).toBeInTheDocument();
-    expect(TopicButtonContainer).toHaveBeenCalledTimes(1);
-    expect(TopicButtonContainer).toHaveBeenCalledWith({}, undefined);
+    expect(SubmitButton).toHaveBeenCalledTimes(1);
+    expect(SubmitButton).toHaveBeenCalledWith(
+      { text: PLAY_AGAIN_BUTTON_TEXT, handleButtonClick: handleResetMock },
+      undefined,
+    );
   });
 
-  it("calls hook useRouterStartPage", (): void => {
+  it("calls handleReset when SubmitButton is triggered", (): void => {
     setup();
 
-    expect(useRouterStartPage).toHaveBeenCalledTimes(1);
-    expect(useRouterStartPage).toHaveBeenCalledWith();
-    expect(useRouterStartPage).toHaveReturnedWith(undefined);
+    const element: HTMLElement = screen.getByTestId(
+      submitButtonContainerDataTestId,
+    );
+    fireEvent.click(element);
+
+    expect(element).toBeInTheDocument();
+    expect(handleResetMock).toHaveBeenCalledTimes(1);
+    expect(handleResetMock).toHaveBeenCalledWith(
+      expect.objectContaining({ _reactName: ReactNamesEnum.ON_CLICK }),
+    );
+  });
+
+  it("calls hook useResetGame", (): void => {
+    setup();
+
+    expect(useResetGame).toHaveBeenCalledTimes(1);
+    expect(useResetGame).toHaveBeenCalledWith();
+    expect(useResetGame).toHaveReturnedWith({ handleReset: handleResetMock });
   });
 });
--- src/pages/ResultPage/ResultPage.tsx
+++ src/pages/ResultPage/ResultPage.tsx
@@ -1,8 +1,36 @@
 import "./ResultPage.scss";
 import React, { ReactElement } from "react";
+import TitleContainer from "@/components/container/TitleContainer/TitleContainer.tsx";
+import {
+  EMPTY_STRING,
+  PLAY_AGAIN_BUTTON_TEXT,
+  RESULTPAGE_HEADLINE_BOLD_TEXT,
+  RESULTPAGE_HEADLINE_TEXT,
+} from "@/globals/constants/Constants.ts";
+import ScoreCard from "@/components/container/ScoreCard/ScoreCard.tsx";
+import SubmitButton from "@/components/atoms/SubmitButton/SubmitButton.tsx";
+import useResetGame from "@/hooks/result/useResetGame.ts";
+import { ResetGameHook } from "@/globals/models/types/ResultTypes.ts";
 
 const ResultPage: React.FC = (): ReactElement => {
-  return <div className="resultPage"></div>;
+  const { handleReset }: ResetGameHook = useResetGame();
+
+  return (
+    <div className="resultPage">
+      <TitleContainer
+        firstLine={RESULTPAGE_HEADLINE_TEXT}
+        headline={RESULTPAGE_HEADLINE_BOLD_TEXT}
+        subLine={EMPTY_STRING}
+      />
+      <div className="resultPageResult">
+        <ScoreCard />
+        <SubmitButton
+          text={PLAY_AGAIN_BUTTON_TEXT}
+          handleButtonClick={handleReset}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ResultPage;
