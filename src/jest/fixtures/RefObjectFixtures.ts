c76611f3678b5a4210b68360cc0d8dc084aff21c
kephalosk <watchfromtheshore@gmail.com>
2025-05-27 20:58:23 +0200

    update fixture

--- src/jest/fixtures/RefObjectFixtures.ts
+++
@@ -1,9 +0,0 @@
-import { mockedDOMRect } from "@/jest/fixtures/DOMRectFixtures.ts";
-import React from "react";
-
-export const mockedRefObjectDiv: React.RefObject<HTMLDivElement> = {
-  current: {
-    offsetWidth: 100,
-    getBoundingClientRect: (): DOMRect => mockedDOMRect,
-  } as HTMLDivElement,
-};
